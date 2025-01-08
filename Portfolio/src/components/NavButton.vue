4c19ed37ad585c81572ee97a6f79893b84ead91c
Jose-Prince <joseprince148@gmail.com>
2025-01-07 18:17:50 -0600

    Style: Added color when component clicked

--- Portfolio/src/components/NavButton.vue
+++ Portfolio/src/components/NavButton.vue
@@ -1,29 +1,64 @@
 <script>
 export default {
-	props: {
-		titlePage: String,
-		page: String,
-	},
-	methods: {
-		navigate() {
-			this.$router.push({ path: `/${this.page}`})
-		}
-	}
-}
+  props: {
+    titlePage: String,
+    page: String,
+  },
+  computed: {
+    isActive() {
+      return `/${this.page}` === this.$route.path;
+    },
+  },
+  methods: {
+    navigate() {
+      this.$router.push({ path: `/${this.page}` });
+    },
+  },
+};
 </script>
 
 <template>
-	<div class="parallelogram" @click="navigate">
-		{{ titlePage }}
-	</div>
+  <div 
+    class="parallelogram" 
+    :class="{ active: isActive }" 
+    @click="navigate"
+  >
+    <p class="textBar" :class="{ active: isActive }">
+      {{ titlePage }}
+    </p>
+  </div>
 </template>
 
 <style>
-	.parallelogram {
-		width: 100px;
-		height: 100%;
-		transform: skew(20deg);
-		background: #555;
-		cursor: pointer;
-	}
+.parallelogram {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 225px;
+  height: 100%;
+  transform: skew(20deg);
+  background: #FFF;
+  cursor: pointer;
+  border-left: 1px solid #000;
+  border-right: 1px solid #000;
+  transition: background 0.3s ease;
+}
+
+.parallelogram:hover {
+  background: #8fa6bc;
+}
+
+.parallelogram.active {
+  background: #4a90e2; 
+}
+
+.textBar {
+  transform: skew(340deg);
+  font-size: 20px;
+  opacity: 100%;
+}
+
+.textBar.active {
+	
+}
 </style>
