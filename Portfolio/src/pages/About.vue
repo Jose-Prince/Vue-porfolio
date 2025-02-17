602fb1eda8445f22e4483b0b2a70d016c7746d1a
Jose-Prince <joseprince148@gmail.com>
2025-02-17 16:28:21 -0600

    Style: Better style for Who Am I text

--- Portfolio/src/pages/About.vue
+++ Portfolio/src/pages/About.vue
@@ -6,7 +6,7 @@
         <div class="top-row">
 			<div class="lower-row">
 				<h1>Who I am?</h1>
-				<!-- <div>I have been working as a programmer for three years and I am currently a  student at the Universidad del Valle de Guatemala, pursuing a degree in  Computer Science and Information Technology Engineering. Although I do  not have formal work experience as a junior programmer, I have extensive knowledge in various programming languages both in the field of  Front-end and Back-end development. My main interest lies in Front-end  development for mobile applications and websites, and I have an  outstanding proficiency in the associated web technologies. I am avid  for knowledge and am constantly exploring new technologies to improve my skills and better understand their application in different contexts.</div> -->
+				<div style="text-align: justify;">I have been working as a programmer for three years and I am currently a  student at the Universidad del Valle de Guatemala, pursuing a degree in  Computer Science and Information Technology Engineering. Although I do  not have formal work experience as a junior programmer, I have extensive knowledge in various programming languages both in the field of  Front-end and Back-end development. My main interest lies in Front-end  development for mobile applications and websites, and I have an  outstanding proficiency in the associated web technologies. I am avid  for knowledge and am constantly exploring new technologies to improve my skills and better understand their application in different contexts.</div>
 			</div>
 			<div class="box-hobbies">
 				<h1>My hobbies:</h1>
@@ -25,18 +25,19 @@
 	flex-direction: column;
 	height: 100%;
 	color: black;
-	padding: 10px;
+	padding: 50px;
   }
 
   .top-row {
 	display: grid;
-	grid-template-columns: auto auto;
+	grid-template-columns: 50% 50%;
 	height: 100%;
   }
 
   .lower-row {
 	display: flex;
 	flex-direction: column;
+    padding-right: 50px;
 	height: 100%;
   }
 
@@ -44,4 +45,5 @@
 	border: 1px solid #B3A0D1;
 	border-radius: 10px;
   }
+
 </style>
